bda1ee656ad031b2275f4e4ed36bcefdb521ea34
rselent <rselent@ufl.edu>
2020-05-28 03:54:43 -0400

    went down pygame rabbit hole D:

--- board.py
+++ board.py
@@ -4,8 +4,10 @@ Game of Life grid (structure, drawing, animating, etc)
 
 
 import matplotlib.pyplot as plt
-import numpy.random as random
+import numpy.random as rng
+import pygame
 from engine import GameEngine
+
 
 import time
 
@@ -14,7 +16,8 @@ import time
 GAME_TITLE = "Tinder: The Game"
 DESIRED_FRAMERATE = .0167		# in seconds -- frame time-> .0333 = 30fps, 
 								#							 .0167 = 60fps
-
+RANDOM_BIAS_ARRAY = [True, False]
+RNG = rng.default_rng()
 
 class DrawGrid:
 	def __init__( self, size):
@@ -24,8 +27,8 @@ class DrawGrid:
 		Arguments:
 			size {int tuple} -- size of grid to draw (x, y)
 		"""
-		# init self state as randomized integer based on size tuple
-		self.state = random.randint( 2, size= size)
+		# init self state as randomized binary value based on given tuple (size)
+		self.state = RNG.integers( 2, size= size)
 		# init self access to GameEngine class
 		self.gameEngine = GameEngine( self)
 	def animateGrid( self):
@@ -64,4 +67,62 @@ class DrawGrid:
 			start = time.time()
 			plt.pause( DESIRED_FRAMERATE)
 			yield self
+
+
+
+class GameWindow:
+	def __init__( self, screen, x, y):
+		vector = pygame.math.Vector2
+		self.screen = screen
+		self.pos = vector( x, y)
+
+#		self.rows = USER INPUT HERE
+#		self.cols = USER INPUT HERE
+#		self.width, self.height = self.rows*20, self.columns*20		# *20 = size of cell
+		self.width, self.height = x*2+2, y*3+2
+		self.image = pygame.Surface( (self.width, self.height))
+		self.rect = self.image.get_rect()
+
+		self.rows = int( self.height/20)
+		self.columns = int( self.width/20)
+		self.grid = [[DrawCells( self.image, x, y) for x in range( self.columns)] for y in range( self.rows)]
+
+
+
+	def update( self):
+		self.rect.topleft = self.pos
+		for row in self.grid:
+			for cell in row:
+				cell.update()
+
+	def draw( self):
+		self.image.fill( (108, 26, 190))
+		for row in self.grid:
+			for cell in row:
+				cell.draw()
+		self.screen.blit( self.image, (self.pos.x, self.pos.y))
+
+
+class DrawCells:
+	def __init__( self, surface, gridX, gridY, randomSeed= True):
+		if randomSeed:
+			self.alive = RNG.choice( RANDOM_BIAS_ARRAY)
+		else:
+			self.alive = False
+		self.surface = surface
+		self.gridX = gridX
+		self.gridY = gridY
+		self.image = pygame.Surface( (20, 20))
+		self.rect = self.image.get_rect()
+
+	def update( self):
+		self.rect.topleft = (self.gridX*20, self.gridY*20)
+
+	def draw( self):
+		if self.alive:
+			self.image.fill( (0, 0, 0))
+		else:
+			self.image.fill( (0, 0, 0))
+			pygame.draw.rect( self.image, (255, 255, 255), (2, 2, 18, 18))
+		self.surface.blit( self.image, (self.gridX*20, self.gridY*20))
 
--- life.py
+++ life.py
@@ -8,20 +8,114 @@ An implementation of Conway's "Game of Life"
 
 """
 
+
+import pygame
+from pygame.locals import *
 from engine import GameEngine
-from board import DrawGrid
+from board import DrawGrid, GameWindow
 
 
-HEIGHT = 192
-WIDTH = HEIGHT
+WIDTH, HEIGHT = 1280, 720
+BACKGROUND = (26, 108, 190)
+FPS_TARGET = 60
+
+
+class Events:
+	def __init__( self):
+		...
+	def mouseOverGrid( self, event):
+		if event[0] > WIDTH/4 and event[0] < WIDTH/4*3:
+			if event[1] > HEIGHT/6 and event[1] < HEIGHT/6*4:
+				return True
+		return False
+	
+	def clickGrid( self, event):
+		gridPos = [event[0]-(WIDTH/4), event[1]-(HEIGHT/6)]
+		gridPos[0] = int( gridPos[0]//20)
+		gridPos[1] = int( gridPos[1]//20)
+		alive = Game._window.grid[gridPos[1]][gridPos[0]].alive
+
+		if alive:
+			Game._window.grid[gridPos[1]][gridPos[0]].alive = False
+		else:
+			Game._window.grid[gridPos[1]][gridPos[0]].alive = True
+
+
+
+
+class Game( Events):
+	_window = None
+	_fps = None
+
+	def __init__( self):
+		self._running = True
+		self._display_surf = None
+		self.size = WIDTH, HEIGHT
+
+	def cleanup( self):
+		pygame.quit()
+
+	def on_init( self):
+		pygame.init()
+		self._display_surf = pygame.display.set_mode( (self.size), 
+									pygame.HWSURFACE | pygame.DOUBLEBUF)
+		self._running = True
+
+		Game._fps = pygame.time.Clock()
+		Game._window = GameWindow( self._display_surf, (WIDTH/4), (HEIGHT/6))
+
+	def on_event( self, event):
+		if event.type == QUIT:
+			self._running = False
+		elif event.type == MOUSEMOTION:
+			...
+		elif event.type == MOUSEBUTTONDOWN:
+			mousePos = pygame.mouse.get_pos()
+			if self.mouseOverGrid( mousePos):
+				self.clickGrid( mousePos)
+		elif event.type == MOUSEBUTTONUP:
+			...
+		elif event.type >= USEREVENT:
+			...
+		elif event.type == ACTIVEEVENT:
+			...
+
+	def loop( self):
+		Game._window.update()
+
+	def render( self):
+		self._display_surf.fill( BACKGROUND)
+		Game._window.draw()
+		pygame.display.flip()
+
+	def on_execute( self):
+		if self.on_init() == False:
+			self._running = False
+
+		while( self._running):
+			for event in pygame.event.get():
+				self.on_event( event)
+				
+			self.render()
+#			self.loop()
+			Game._fps.tick( FPS_TARGET)
+			print( "framerate: {:.2f}\t\t".format( Game._fps.get_fps()), end= "\r")
+
+		self.cleanup()
+
 
 
 # main game loop?
 def main():
-	...
-	grid = DrawGrid(( HEIGHT, WIDTH))
-	for _ in grid.animateGrid():
-		...
+
+	gameoflife = Game()
+
+	gameoflife.on_init()
+	gameoflife.on_execute()
+	
+	#grid = DrawGrid(( HEIGHT, WIDTH))
+	#for _ in grid.animateGrid():
+	#	...
 
 
 if __name__ == "__main__":
